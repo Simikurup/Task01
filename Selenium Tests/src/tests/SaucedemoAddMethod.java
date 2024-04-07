376e0e0708b81dce172ba4faafe16cc7ed30687c
rohan <rohan@10.0.0.241>
2024-04-07 13:22:29 +1000

    Updated code

--- Selenium Tests/src/tests/SaucedemoAddMethod.java
+++ Selenium Tests/src/tests/SaucedemoAddMethod.java
@@ -16,6 +16,15 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class SaucedemoAddMethod {
+	private static final String SHIPPINGINFO_TEXT = "Free Pony";
+	private static final String PAYMENTINFO_TEXT = "SauceCard";
+	private static final String CHECKOUT_COMPLETE = "https://www.saucedemo.com/checkout-complete.html";
+	private static final String CHECKOUT_STEPTWO = "https://www.saucedemo.com/checkout-step-two.html";
+	private static final String CHECKOUT_STEPONE = "https://www.saucedemo.com/checkout-step-one.html";
+	private static final String SHOPPINGCART_PAGE = "https://www.saucedemo.com/cart.html";
+	private static final String DESCRIPTION = "carry.allTheThings()";
+	private static final String TITLE = "Sauce Labs";
+	private static final String PRODUCTS_PAGE = "https://www.saucedemo.com/inventory.html";
 	private static final String POSTALCODE = "v4s 213";
 	private static final String LASTNAME = "kurup";
 	private static final String FIRSTNAME = "Simi";
@@ -64,194 +73,203 @@ public class SaucedemoAddMethod {
 
 	@BeforeMethod
 	public void setUp() {
-	driver=new ChromeDriver();
-	wait= new WebDriverWait(driver,Duration.ofSeconds(30));
-	driver.manage().window().maximize();
+		driver=new ChromeDriver();
+		wait= new WebDriverWait(driver,Duration.ofSeconds(30));
+		driver.manage().window().maximize();
 	}
 
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
-		
+
 	}
 
 	@Test
-	public void shopping() throws InterruptedException {
-		homePageUrl(HOME_PAGE_URL );
+	public void saucedemoShopping() {
+		openHomePage();
 		Assert.assertEquals(getHomePageUrl(), HOME_PAGE_URL);
-		
-		customerUsername(USER_NAME);
-		
-		customerPassword(PASSWORD);
-		
-		customerLogin();
-		
+
+		typeCustomerUsername(USER_NAME);
+		typeCustomerPassword(PASSWORD);
+		login();
+
 		//Verify there are 6 products in the page
-	/*	By inventorylist_Locator=By.xpath("//div[@class='inventory_list']");//2
+		/*	By inventorylist_Locator=By.xpath("//div[@class='inventory_list']");//2
 		List <WebElement>Inventory=driver.findElements(inventorylist_Locator);
 		int size=Inventory.size();
 		System.out.println(size);*/
-		
-		
-		Assert.assertTrue(getProductTitle().startsWith("Sauce Labs"));
-		Assert.assertTrue(getProductDescription().startsWith("carry.allTheThings()"));
+
+		Assert.assertTrue(driver.getCurrentUrl().startsWith(PRODUCTS_PAGE));
+		Assert.assertTrue(getProductTitle().startsWith(TITLE));
+		Assert.assertTrue(getProductDescription().startsWith(DESCRIPTION));
 		Assert.assertTrue(getProductPrice()>0);
-		
 		addProductToCart();
-			
-		Assert.assertTrue(getNumberOfItemsInCart()==1);	
+		
+		Assert.assertEquals(getNumberOfItemsInCart(), 1);
 		
 		goToShoppingCart();
-		
-		Assert.assertTrue(getProductTitle().startsWith("Sauce Labs"));
-		Assert.assertTrue(getProductDescription().startsWith("carry.allTheThings()"));
+
+		Assert.assertEquals(driver.getCurrentUrl(), SHOPPINGCART_PAGE);
+		Assert.assertTrue(getProductTitle().startsWith(TITLE));
+		Assert.assertTrue(getProductDescription().startsWith(DESCRIPTION));
 		Assert.assertTrue(getProductPrice()>0);
-		
 		checkout();
+
+		Assert.assertEquals(driver.getCurrentUrl(), CHECKOUT_STEPONE);
+		typeFirstName(FIRSTNAME);
+		typeLastName(LASTNAME);
+		typePostcode(POSTALCODE);
+		continueToNextPage();
 		
-		
-		checkOutFirstName(FIRSTNAME);
-		checkOutLastName(LASTNAME);
-		checkOutPostcode(POSTALCODE);
-		checkOutContinue();
-		
-		Assert.assertTrue(getProductTitle().startsWith("Sauce Labs"));
-		Assert.assertTrue(getProductDescription().startsWith("carry.allTheThings()"));
+		Assert.assertEquals(driver.getCurrentUrl(), CHECKOUT_STEPTWO);
+		Assert.assertTrue(getProductTitle().startsWith(TITLE));
+		Assert.assertTrue(getProductDescription().startsWith(DESCRIPTION));
 		Assert.assertTrue(getProductPrice()>0);
-	
-		
-		Assert.assertTrue(getPaymentInfo().startsWith("SauceCard"));
-		Assert.assertTrue(getShippingInfo().startsWith("Free Pony"));
+		Assert.assertTrue(getPaymentInfo().startsWith(PAYMENTINFO_TEXT));
+		Assert.assertTrue(getShippingInfo().startsWith(SHIPPINGINFO_TEXT));
 		Assert.assertTrue(getPriceTotal()>0);
 		Assert.assertTrue(getTax()>0);
 		Assert.assertTrue(getTotal()>0);
 		finishCheckOut();
-			
+		
+		Assert.assertEquals(driver.getCurrentUrl(), CHECKOUT_COMPLETE);
 		Assert.assertTrue(getcheckOutompleteText().startsWith("Your order"));
-	
+
+
+
+	}
+	private void openHomePage() {
+		driver.get(HOME_PAGE_URL );
+	}
+
+	private String getHomePageUrl() {
+		String currentUrl=driver.getCurrentUrl();
+		return currentUrl;
+	}
+
+	private void typeCustomerUsername(String uname) {
+		wait.until(ExpectedConditions.elementToBeClickable(USERNAME_LOCATOR));
+		WebElement usernameTextBox=driver.findElement(USERNAME_LOCATOR);
+		usernameTextBox.sendKeys(uname);
+	}
+
+	private void typeCustomerPassword(String password) {	
+		wait.until(ExpectedConditions.elementToBeClickable(PASSWORD_LOCATOR));
+		WebElement passwordTextBox=driver.findElement(PASSWORD_LOCATOR);
+		passwordTextBox.sendKeys(password);
+	}	
+	private void login(){	
+		wait.until(ExpectedConditions.elementToBeClickable(LOGIN_BUTTON_LOCATOR));
+		WebElement loginButton=driver.findElement(LOGIN_BUTTON_LOCATOR);
+		loginButton.click();
+	}
+	private String getProductTitle() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(PRODUCT_TITLE_LOCATOR));
+		WebElement productTitleLabel=driver.findElement(PRODUCT_TITLE_LOCATOR);
+		return productTitleLabel.getText();
+	}
+	private String getProductDescription() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(PRODUCT_DESCRIPTION_LOCATOR));
+		WebElement productDescriptionText=driver.findElement(PRODUCT_DESCRIPTION_LOCATOR);
+		return productDescriptionText.getText();
+	}
+
+	private double getProductPrice() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(PRODUCT_PRICE_LOCATOR));
+		WebElement productPriceText=driver.findElement(PRODUCT_PRICE_LOCATOR);
+		String priceValue=productPriceText.getText();
+		String priceReplace$=priceValue.replace("$", "");
+		double price=Double.parseDouble(priceReplace$);
+		return price;
+	}
+	private void addProductToCart() {
+		wait.until(ExpectedConditions.elementToBeClickable(ADD_TOCART_LOCATOR));
+		WebElement addToCartButton = driver.findElement(ADD_TOCART_LOCATOR);
+		addToCartButton.click();
+	}
+	private int getNumberOfItemsInCart() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(SHOPPING_CART_LOCATOR));
+		WebElement shoppingCartLabel=driver.findElement(SHOPPING_CART_LOCATOR);
+		String cartValue=shoppingCartLabel.getText();
+		int cartItems=Integer.parseInt(cartValue);
+		return cartItems;
+				
+	}
+	private void goToShoppingCart() {
+		wait.until(ExpectedConditions.elementToBeClickable(SHOPPING_CART_LOCATOR));
+		WebElement shoppingCartLabel=driver.findElement(SHOPPING_CART_LOCATOR);
+		shoppingCartLabel.click();
+	}
+	private void checkout() {
+		wait.until(ExpectedConditions.elementToBeClickable(CHECKOUT_LOCATOR));
+		WebElement checkoutButton=driver.findElement(CHECKOUT_LOCATOR);
+		checkoutButton.click();
+	}
+	private void typeFirstName(String firstname) {
+		wait.until(ExpectedConditions.elementToBeClickable(FIRSTNAME_LOCATOR));
+		WebElement firstNameTextBox =driver.findElement(FIRSTNAME_LOCATOR);
+		firstNameTextBox.sendKeys(firstname);
+	}
+	private void typeLastName(String lastname) {
+		wait.until(ExpectedConditions.elementToBeClickable(LASTNAME_LOCATOR));
+		WebElement lastNameTextBox =driver.findElement(LASTNAME_LOCATOR);
+		lastNameTextBox.sendKeys(lastname);
+	}
+	private void typePostcode(String postcode) {
+		wait.until(ExpectedConditions.elementToBeClickable(FIRSTNAME_LOCATOR));
+		WebElement postCodeTextBox =driver.findElement(POSTALCODE_LOCATOR);
+		postCodeTextBox.sendKeys(postcode);
+	}
+	private void continueToNextPage() {
+		wait.until(ExpectedConditions.elementToBeClickable(CONTINUE_BUTTON_LOCATOR));
+		WebElement continueButton =driver.findElement(CONTINUE_BUTTON_LOCATOR);
+		continueButton.click();
+	}
+
+	private String getPaymentInfo() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(PAYMENTINFO_LOCATOR));
+		WebElement paymentInfoLabel=driver.findElement(PAYMENTINFO_LOCATOR);
+		return paymentInfoLabel.getText();
+	}
+	private String getShippingInfo() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(SHIPPINGINFO_LOCATOR));
+		WebElement shippingInfoLabel=driver.findElement(SHIPPINGINFO_LOCATOR);
+		return shippingInfoLabel.getText();
+	}
+	private double getPriceTotal() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(ITEMTOTAL_LOCATOR));
+		WebElement itemTotalLabel=driver.findElement(ITEMTOTAL_LOCATOR);
+		String itemValue=itemTotalLabel.getText();
+		String itemValueReplace$=itemValue.replace("Item total: $", "");
+		double totalValue=Double.parseDouble(itemValueReplace$);
+		return totalValue;
+	}
+	private double getTax() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(TAX_LOCATOR));
+		WebElement taxLabel=driver.findElement(TAX_LOCATOR);
+		String taxValue=taxLabel.getText();
+		String taxValueReplace$=taxValue.replace("Tax: $", "");
+		double taxToPay=Double.parseDouble(taxValueReplace$);
+		return taxToPay;
+	}
+	private double getTotal() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(TOTAL_LOCATOR));
+		WebElement totalLabel=driver.findElement(TOTAL_LOCATOR);
+		String totalVal=totalLabel.getText();
+		String totalValReplace$=totalVal.replace("Total: $", "");
+		double totalToPay=Double.parseDouble(totalValReplace$);
+		return totalToPay;
+	}
+	private void finishCheckOut() {
+		wait.until(ExpectedConditions.elementToBeClickable(FINISH_LOCATOR));
+		WebElement finishButton=driver.findElement(FINISH_LOCATOR);
+		finishButton.click();
+	}
+	private String getcheckOutompleteText() {
+		wait.until(ExpectedConditions.visibilityOfElementLocated(CHECKOUT_DESCRIPTION_LOCATOR));
+		WebElement CheckOutDescriptionText=driver.findElement(CHECKOUT_DESCRIPTION_LOCATOR);
+		return CheckOutDescriptionText.getText();
 		
-		
 	}
-private void homePageUrl(String url) {
-	driver.get(url);
-}
-
-private String getHomePageUrl() {
-	String currentUrl=driver.getCurrentUrl();
-	return currentUrl;
-}
-
-private void customerUsername(String uname) {
-	wait.until(ExpectedConditions.elementToBeClickable(USERNAME_LOCATOR));
-	WebElement usernameLabel=driver.findElement(USERNAME_LOCATOR);
-	usernameLabel.sendKeys(uname);
-}
-	
-private void customerPassword(String password) {	
-	wait.until(ExpectedConditions.elementToBeClickable(PASSWORD_LOCATOR));
-	WebElement passwordLabel=driver.findElement(PASSWORD_LOCATOR);
-	passwordLabel.sendKeys(password);
-}	
-private void customerLogin(){	
-	wait.until(ExpectedConditions.elementToBeClickable(LOGIN_BUTTON_LOCATOR));
-	WebElement loginButton=driver.findElement(LOGIN_BUTTON_LOCATOR);
-	loginButton.click();
-}
-private String getProductTitle() {
-	WebElement product_Title=driver.findElement(PRODUCT_TITLE_LOCATOR);
-	String title=product_Title.getText();
-	return title;
-	
-}
-
-private String getProductDescription() {
-WebElement product_Description=driver.findElement(PRODUCT_DESCRIPTION_LOCATOR);
-String description=product_Description.getText();
-return description;
-}
-
-private double getProductPrice() {
-WebElement product_Price=driver.findElement(PRODUCT_PRICE_LOCATOR);
-String priceValue=product_Price.getText();
-String priceReplace$=priceValue.replace("$", "");
-double price=Double.parseDouble(priceReplace$);
-return price;
-}
-private void addProductToCart() {
-	wait.until(ExpectedConditions.elementToBeClickable(ADD_TOCART_LOCATOR));
-	WebElement addToCart = driver.findElement(ADD_TOCART_LOCATOR);
-	addToCart.click();
-}
-private int getNumberOfItemsInCart() {
-	WebElement shoppingCart=driver.findElement(SHOPPING_CART_LOCATOR);
-	String cartValue=shoppingCart.getText();
-	int cartItems=Integer.parseInt(cartValue);
-	return cartItems;
-	}
-private void goToShoppingCart() {
-	WebElement shoppingCart=driver.findElement(SHOPPING_CART_LOCATOR);
-	shoppingCart.click();
-}
-private void checkout() {
-	WebElement checkout=driver.findElement(CHECKOUT_LOCATOR);
-	checkout.click();
-}
-private void checkOutFirstName(String firstname) {
-	WebElement firstName =driver.findElement(FIRSTNAME_LOCATOR);
-	firstName.sendKeys(firstname);
-}
-private void checkOutLastName(String lastname) {
-WebElement lastName =driver.findElement(LASTNAME_LOCATOR);
-lastName.sendKeys(lastname);
-}
-private void checkOutPostcode(String postcode) {
-WebElement postCode =driver.findElement(POSTALCODE_LOCATOR);
-postCode.sendKeys(postcode);
-}
-private void checkOutContinue() {
-WebElement continueButton =driver.findElement(CONTINUE_BUTTON_LOCATOR);
-continueButton.click();
-}
-
-private String getPaymentInfo() {
-	WebElement paymentInfo=driver.findElement(PAYMENTINFO_LOCATOR);
-	String paymentDetails=paymentInfo.getText();
-	return paymentDetails;
-	}
-private String getShippingInfo() {
-	WebElement shippingInfo=driver.findElement(SHIPPINGINFO_LOCATOR);
-	String shippingDetails=shippingInfo.getText();
-	return shippingDetails;
-	}
-private double getPriceTotal() {
-	WebElement itemTotal=driver.findElement(ITEMTOTAL_LOCATOR);
-	String itemValue=itemTotal.getText();
-	String itemValueReplace$=itemValue.replace("Item total: $", "");
-	double totalValue=Double.parseDouble(itemValueReplace$);
-	return totalValue;
-}
-private double getTax() {
-	WebElement tax=driver.findElement(TAX_LOCATOR);
-	String taxValue=tax.getText();
-	String taxValueReplace$=taxValue.replace("Tax: $", "");
-	double taxToPay=Double.parseDouble(taxValueReplace$);
-	return taxToPay;
-	}
-private double getTotal() {
-	WebElement total=driver.findElement(TOTAL_LOCATOR);
-	String totalVal=total.getText();
-	String totalValReplace$=totalVal.replace("Total: $", "");
-	double totalToPay=Double.parseDouble(totalValReplace$);
-	return totalToPay;
-}
-private void finishCheckOut() {
-	WebElement finishButton=driver.findElement(FINISH_LOCATOR);
-	finishButton.click();
-}
-private String getcheckOutompleteText() {
-WebElement checkOut=driver.findElement(CHECKOUT_DESCRIPTION_LOCATOR);
-String checkOutText=checkOut.getText();
-return checkOutText;
-}
 
 }
